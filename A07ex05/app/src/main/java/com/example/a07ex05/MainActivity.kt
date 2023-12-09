3986347b66d0dba85a737d441499ddead70e500b
FabioPortella <fabioportella2009@gmail.com>
2023-12-09 18:52:47 -0300

    Exercicio 05 .

--- A07ex05/app/src/main/java/com/example/a07ex05/MainActivity.kt
+++ A07ex05/app/src/main/java/com/example/a07ex05/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.a07ex05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imgFoto)
+        Glide.with(this)
+            .load("http://pudim.com.br/pudim.jpg")
+            .into(imageView)
     }
+
+    // Alteração do código:
+    // inseri um TextView com a mensagem "Minha nova imagem".
 }
